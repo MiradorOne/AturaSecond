1859c192962ae4f508ed75172dd44b27344b36e4
kitdemonio <ddd48533@gmail.com>
2016-04-15 21:15:46 +0200

    Update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+/*Owl carousel*/
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      600:{
+        items:1,
+        lup:true,
+        nav:false,
+        fallbackEasing: true,
+      },
+      1000:{
+        items:1,
+        nav:true,
+        loop:true,
+        dots:true,
+        animateIn: true,
+      }
+    }
+  });
+
+});
